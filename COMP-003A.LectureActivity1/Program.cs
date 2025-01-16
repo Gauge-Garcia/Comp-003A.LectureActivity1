dc1530ace59cd37fd78f2bcd9488e0a155816d4e
garci <garci@TABLET-5LB2R766>
2025-01-15 19:59:04 -0800

    Add project files.

---
+++ COMP-003A.LectureActivity1/Program.cs
@@ -0,0 +1,13 @@
+﻿//namespace level
+namespace Comp_003A.LectureActivity1
+{
+    // Class Level
+    internal class Program
+    {
+        // main level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
